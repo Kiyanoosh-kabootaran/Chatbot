ab48e035f4173ba12e75b6ed39d629b4ee026e26
Kiyanoosh Kabootaran <kkabootaran@gmail.com>
2025-09-24 17:03:56 +0330

    get user input and print in the console

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const API_KEY = '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';
 
+const sendButton = document.getElementById('sendButton');
+const chatInput = document.getElementById('chatInput');
+
+sendButton.addEventListener('click', ()=> handleSendMessage());
+
+chatInput.addEventListener('keypress' , event =>{
+    if(event.key === 'Enter'){
+      handleSendMessage();
+    }
+  })
+
+function handleSendMessage(){
+  const question = chatInput.value.trim();
+  console.log(question);
+}
+
 const fetchData = fetch("https://openrouter.ai/api/v1/chat/completions", {
   method: "POST",
   headers: {
     "Authorization": `Bearer ${API_KEY}`,
-    "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
-    "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
     "Content-Type": "application/json"
   },
   body: JSON.stringify({
@@ -19,5 +33,5 @@ const fetchData = fetch("https://openrouter.ai/api/v1/chat/completions", {
   })
 });
 
-fetchData.then(response => response.json())
-  .then(data => console.log(data.choices[0].message.content));
+//fetchData.then(response => response.json())
+ // .then(data => console.log(data.choices[0].message.content));
