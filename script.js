d413172d81273d96fc952fac9b2cfbd0548fa408
Kiyanoosh Kabootaran <kkabootaran@gmail.com>
2025-09-25 12:57:06 +0330

    scroll to the bottom when we send question

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ function getAnswer(question){
       isAnswerLoading = false;
       addAnswerSection(resultData);
     }).then(() =>{
+      scrollBotton();
       sendButton.classList.remove('send-button-noneactive');
     });
     }
@@ -70,6 +71,7 @@ function getAnswer(question){
   content.appendChild(sectionElement);
   //Add answer section after added question section
   addAnswerSection(message);
+  scrollBotton();
  }
 
  function addAnswerSection(message){
@@ -93,4 +95,11 @@ function getAnswer(question){
 
  function getloadingSvg(){
   return '<svg style="height: 25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="#4F6BFE" stroke="#4F6BFE" stroke-width="15" r="15" cx="40" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle><circle fill="#4F6BFE" stroke="#4F6BFE" stroke-width="15" r="15" cx="100" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle><circle fill="#4F6BFE" stroke="#4F6BFE" stroke-width="15" r="15" cx="160" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></circle></svg>'
+ }
+
+ function scrollBotton() {
+  content.scrollTo({
+    top: content.scrollHeight,
+    behavior: 'smooth'
+  })
  }
